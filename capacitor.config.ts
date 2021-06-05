7b124a5654bedec7d6f17f61113bbf053f7175bf
Abhishek Raj <abhishek@betatestsolutions.com>
2021-06-05 13:55:00 +0530

    Add capacitor to project with Android.

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.techoutopia.webstatus',
+  appName: 'WebStatus',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
