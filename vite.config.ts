b4541271faaca303c8e7eb226e7d8e23baf25846
Abhishek Raj <abhishek@betatestsolutions.com>
2023-05-02 12:09:49 +0530

    Update config for build folder

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgrPlugin from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "./dist",
+    outDir: "build",
   },
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
 });
